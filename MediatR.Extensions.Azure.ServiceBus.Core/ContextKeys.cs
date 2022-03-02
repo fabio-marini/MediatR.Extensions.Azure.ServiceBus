4e83f10b95da934165ecf2786b68a3e857c9ed11
Fabio Marini <f.marini@gmail.com>
2022-03-02 18:46:59 +0100

    EnqueueTime is still using the pipeline context! :)

---
+++ MediatR.Extensions.Azure.ServiceBus.Core/ContextKeys.cs
@@ -0,0 +1,8 @@
+﻿namespace MediatR.Extensions.Azure.ServiceBus
+{
+    public class ContextKeys
+    {
+        public const string EnqueueTimeUtc = "EnqueueTimeUtc";
+        public const string SequenceNumbers = "SequenceNumbers";
+    }
+}
